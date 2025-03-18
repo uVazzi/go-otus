d4aa577d26a7cb56b369378cce2431786789f466
v.balandin <uvazzi@gmail.com>
2025-03-19 01:35:43 +0700

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,72 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrRequiredPath          = errors.New("fromPath and toPath must be specified")
+	ErrNotValidOffsetOrLimit = errors.New("offset and limit must be greater than zero")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" || toPath == "" {
+		return ErrRequiredPath
+	}
+	if offset < 0 || limit < 0 {
+		return ErrNotValidOffsetOrLimit
+	}
+
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if limit == 0 {
+		limit = fileInfo.Size()
+	}
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if !fileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	inputFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	_, err = inputFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	outputFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	copySize := fileInfo.Size() - offset
+	if limit < copySize {
+		copySize = limit
+	}
+
+	bar := pb.Full.Start64(copySize)
+	defer bar.Finish()
+
+	reader := io.LimitReader(inputFile, copySize)
+	barReader := bar.NewProxyReader(reader)
+	_, err = io.CopyN(outputFile, barReader, copySize)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,93 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func copyToTemp(t *testing.T, fromPath string, offset, limit int64) error {
+	t.Helper()
+	output, err := os.CreateTemp(os.TempDir(), "output")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err = os.Remove(output.Name())
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	return Copy(fromPath, output.Name(), offset, limit)
+}
+
+func checkCopyFile(t *testing.T, checkPath string, offset, limit int64) {
+	t.Helper()
+	fromPath := "testdata/input.txt"
+	output, err := os.CreateTemp(os.TempDir(), "output")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err = os.Remove(output.Name())
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	err = Copy(fromPath, output.Name(), offset, limit)
+	require.NoError(t, err)
+
+	outputFile, err := os.ReadFile(output.Name())
+	require.NoError(t, err)
+
+	checkedFile, err := os.ReadFile(checkPath)
+	require.NoError(t, err)
+
+	require.Equal(t, checkedFile, outputFile)
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("fromPath and toPath must be specified", func(t *testing.T) {
+		err := copyToTemp(t, "", 0, 0)
+		require.Truef(t, errors.Is(err, ErrRequiredPath), "actual err - %v", err)
+
+		err = Copy("testdata/input.txt", "", 0, 0)
+		require.Truef(t, errors.Is(err, ErrRequiredPath), "actual err - %v", err)
+	})
+
+	t.Run("offset and limit must be greater than zero", func(t *testing.T) {
+		err := copyToTemp(t, "testdata/input.txt", -100, 0)
+		require.Truef(t, errors.Is(err, ErrNotValidOffsetOrLimit), "actual err - %v", err)
+
+		err = copyToTemp(t, "testdata/input.txt", 0, -100)
+		require.Truef(t, errors.Is(err, ErrNotValidOffsetOrLimit), "actual err - %v", err)
+	})
+
+	t.Run("not exist file", func(t *testing.T) {
+		err := copyToTemp(t, "testdataNotFound/input.txt", 0, 0)
+		require.ErrorIs(t, err, os.ErrNotExist)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := copyToTemp(t, "testdata/input.txt", 7000, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := copyToTemp(t, "/dev/urandom", 0, 0)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	})
+
+	t.Run("success by testdata", func(t *testing.T) {
+		checkCopyFile(t, "testdata/out_offset0_limit0.txt", 0, 0)
+		checkCopyFile(t, "testdata/out_offset0_limit10.txt", 0, 10)
+		checkCopyFile(t, "testdata/out_offset0_limit1000.txt", 0, 1000)
+		checkCopyFile(t, "testdata/out_offset0_limit10000.txt", 0, 10000)
+		checkCopyFile(t, "testdata/out_offset100_limit1000.txt", 100, 1000)
+		checkCopyFile(t, "testdata/out_offset6000_limit1000.txt", 6000, 1000)
+	})
 }
